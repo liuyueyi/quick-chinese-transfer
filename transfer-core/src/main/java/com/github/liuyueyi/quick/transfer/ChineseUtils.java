355f286da7f790279d12a83ddad51578e2b8c7b4
yihui <bangzewu@126.com>
2020-11-30 20:02:19 +0800

    fix 十，猛繁转简问题

--- transfer-core/src/main/java/com/github/liuyueyi/quick/transfer/ChineseUtils.java
+++ transfer-core/src/main/java/com/github/liuyueyi/quick/transfer/ChineseUtils.java
@@ -22,22 +22,52 @@ public class ChineseUtils {
         return DictionaryContainer.getInstance().getDictionary("s2t").convert(content);
     }
 
+    /**
+     * 简体转香港繁体
+     *
+     * @param content
+     * @return
+     */
     public static String s2hk(String content) {
         return DictionaryContainer.getInstance().getDictionary("s2hk").convert(content);
     }
 
+    /**
+     * 简体转台湾繁体
+     *
+     * @param content
+     * @return
+     */
     public static String s2tw(String content) {
         return DictionaryContainer.getInstance().getDictionary("s2tw").convert(content);
     }
 
+    /**
+     * 繁体转简体
+     *
+     * @param content
+     * @return
+     */
     public static String t2s(String content) {
         return DictionaryContainer.getInstance().getDictionary("t2s").convert(content);
     }
 
+    /**
+     * 香港繁体转简体
+     *
+     * @param content
+     * @return
+     */
     public static String hk2s(String content) {
         return DictionaryContainer.getInstance().getDictionary("hk2s").convert(content);
     }
 
+    /**
+     * 台湾繁体转简体
+     *
+     * @param content
+     * @return
+     */
     public static String tw2s(String content) {
         return DictionaryContainer.getInstance().getDictionary("tw2s").convert(content);
     }
